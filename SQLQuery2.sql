3de1df542c3d4d283be68de876fe3e61d9ee4259
Akhil <akhilu5042@gmail.com>
2022-02-21 08:18:07 +0530

    UC2_CreatedTable

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -4,3 +4,13 @@
   Use Employee_PayRoll_Service
   select * from Employee_PayRoll_Service
 
+ --UC 2 Created Table
+  Create table Employee_PayRoll_Service
+  (
+   Id int identity(100,1),
+   Name varchar(150) NOT NULL,
+   Salary INT NOT NULL,
+   Start DATE NOT NULL,
+   PRIMARY KEY (Id)
+  )
+  DROP TABLE Employee_PayRoll_Service
