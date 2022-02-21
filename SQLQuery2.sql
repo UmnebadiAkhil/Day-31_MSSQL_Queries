baaa125d7b40d0a0a06d2c58a80ecea36639cb3d
Akhil <akhilu5042@gmail.com>
2022-02-21 08:28:08 +0530

    UC 6 Adding Feild Gender & name

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -23,3 +23,18 @@
 
  --UC 4 Ability to retrieve all the employee payroll data that is added to payroll service database
  SELECT * FROM Employee_PayRoll_Service
+
+ --UC 5 To find Data By parmeter & change by CAST
+  SELECT Salary FROM Employee_PayRoll_Service WHERE Name='Bill'
+  SELECT * FROM Employee_PayRoll_Service
+		WHERE Start BETWEEN CAST('2018-01-01' AS DATE) AND GETDATE()
+
+ --UC 6 Adding Feild Gender & name
+
+  Alter table Employee_PayRoll_Service add gender char(1);
+  UPDATE Employee_PayRoll_Service SET gender = 'F' WHERE name='Terisa';
+  UPDATE Employee_PayRoll_Service SET gender = 'M' WHERE name='Bill';
+  UPDATE Employee_PayRoll_Service SET gender = 'M' WHERE name='Charlie';
+
+  select * from Employee_PayRoll_Service 
+
